9d36d5a20e94ed6aebc4022720310ea3a724ca2e
Daniel Evans <me@daredoes.work>
2024-02-23 17:01:00 -0800

    Remove files (#3)
    * remove files
    
    * small trim
    
    * trim 300 megs
    
    * remove junk installs

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 FILE=/config/snapserver.conf
 if test -f "$FILE"; then
     echo "$FILE exists, copying to config folder."
-    cp $FILE /etc/snapserver.conf
+    cat $FILE > /etc/snapserver.conf
 fi
 
 # nqptp &
